e78cb6154678075938ed4f209b323aabc0e4a66c
Kiana <salmasikiana@gmail.com>
2023-06-05 12:10:23 -0600

    Created a variable called pet to ask the user what is their favourite pet

--- Section 1/project1.py
+++ Section 1/project1.py
@@ -2,4 +2,7 @@
 print("Hi welcome to the Band!")
 
 #2. Ask the user for the city that they grew up in.
-city = input("Which city you were born?")
+city = input("Which city you were born? ")
+
+#3. Ask the user for the name of a pet.
+pet = input("What is your pet name? ")
