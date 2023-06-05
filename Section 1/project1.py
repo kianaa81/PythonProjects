30ad29063fcb8ce932972bad767c830357c4ee59
Kiana <salmasikiana@gmail.com>
2023-06-05 12:13:21 -0600

    Combine the city and pet names and print them out as a name of the band

--- Section 1/project1.py
+++ Section 1/project1.py
@@ -6,3 +6,6 @@ city = input("Which city you were born? ")
 
 #3. Ask the user for the name of a pet.
 pet = input("What is your pet name? ")
+
+#4. Combine the name of their city and pet and show them their band name.
+print("The name of your Band could be: " + city + "" + pet)
