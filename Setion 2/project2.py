e821f82ee183cfa74ce62f62cb858e6272e0a660
Kiana <salmasikiana@gmail.com>
2023-06-06 13:06:20 -0600

    Do the calculation to calculate how much each person should pay

--- Setion 2/project2.py
+++ Setion 2/project2.py
@@ -4,3 +4,8 @@ print("Welcome to the tip calculator!")
 bill = float(input("What is i the total of the bill? "))
 tip_percentage = int(input("What percentage tip would you like to give? 10, 12, 0r 15?"))
 people_num = int(input("How many people to split the bill?"))
+
+tip_as_percent = tip_percentage / 100
+total_tip_amount = bill * tip_as_percent
+total_bill = bill + total_tip_amount
+bill_per_person = total_bill / people_num
