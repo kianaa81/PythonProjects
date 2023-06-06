7ff6f2078086ea51ece04d8bdc8317515efe4c8a
Kiana <salmasikiana@gmail.com>
2023-06-06 13:07:37 -0600

    Formatting the final amount to 2 decimal places and print out the results

--- Setion 2/project2.py
+++ Setion 2/project2.py
@@ -9,3 +9,7 @@ tip_as_percent = tip_percentage / 100
 total_tip_amount = bill * tip_as_percent
 total_bill = bill + total_tip_amount
 bill_per_person = total_bill / people_num
+
+#Format the result to 2 decimal places = 33.60
+final_amount = "{:.2f}".format(bill_per_person)
+print(f"Each person should pay {final_amount}")
