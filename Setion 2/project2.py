8cf6a5c7061d8ad37c747775f9166df35ac7e1c8
Kiana <salmasikiana@gmail.com>
2023-06-06 13:02:15 -0600

    Ask the user about how much they want to tip

--- Setion 2/project2.py
+++ Setion 2/project2.py
@@ -2,3 +2,4 @@
 print("Welcome to the tip calculator!")
 
 bill = float(input("What is i the total of the bill? "))
+tip_percentage = int(input("What percentage tip would you like to give? 10, 12, 0r 15?"))
