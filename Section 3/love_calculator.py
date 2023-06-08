bd1628b0bac94807272ed7bdea4e377d975b05ac
Kiana <salmasikiana@gmail.com>
2023-06-07 22:29:13 -0600

    Wrote some lines of code that take both names and make them to be lower case

--- Section 3/love_calculator.py
+++ Section 3/love_calculator.py
@@ -21,8 +21,8 @@ print("Welcome to the Love Calculator!")
 name1 = input("What is your name? \n")
 name2 = input("What is their name? \n")
 
-
-
+combined_string = name1 + name2
+combined_string_lower = combined_string.lower()
 
 
 
