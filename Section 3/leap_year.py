2df58e096fa5dc96a257830a2f1bb3075cf23b25
Kiana <salmasikiana@gmail.com>
2023-06-07 21:41:44 -0600

    Write a code that asks the user put the year. Wrote an if statement to check if the year is divisible by 4 if not then it is not a leap year

--- Section 3/leap_year.py
+++ Section 3/leap_year.py
@@ -7,7 +7,12 @@
 #    **except** every year that is evenly divisible by 100 
 
 #    **unless** the year is also evenly divisible by 400
+year = int(input("Which year do you want to check? "))
 
+if year % 4 == 0:
+
+else:
+  print("Not leap year.")
 
 
 
