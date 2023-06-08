6189f1017fffea9c4e91725333e20eda33396859
Kiana <salmasikiana@gmail.com>
2023-06-07 22:25:23 -0600

    Print out a welcome title Ask about 2 people's names

--- Section 3/love_calculator.py
+++ Section 3/love_calculator.py
@@ -17,7 +17,9 @@
 # Otherwise, the message will just be their score. e.g.:
 # "Your score is **z**."
 
-
+print("Welcome to the Love Calculator!")
+name1 = input("What is your name? \n")
+name2 = input("What is their name? \n")
 
 
 
