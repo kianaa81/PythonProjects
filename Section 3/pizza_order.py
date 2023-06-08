7aaa2b46f7c152051b3ebc060c9f7f6d70c8bb3b
Kiana <salmasikiana@gmail.com>
2023-06-07 21:49:11 -0600

    Create a file to start a new project for pizza order

---
+++ Section 3/pizza_order.py
@@ -0,0 +1,13 @@
+# Based on a user's order, work out their final bill.
+
+# Small Pizza: $15
+
+# Medium Pizza: $20
+
+# Large Pizza: $25
+
+# Pepperoni for Small Pizza: +$2
+
+# Pepperoni for Medium or Large Pizza: +$3
+
+# Extra cheese for any size pizza: + $1
