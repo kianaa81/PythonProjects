0e3e6f1cbbc88e5934db4abd8cadd1af81031257
Kiana <salmasikiana@gmail.com>
2023-06-07 21:52:22 -0600

    Wrote some lines of code that ask the user about the size of their pizza, if they want pepperoni or not, and whether they want extra cheese or not

--- Section 3/pizza_order.py
+++ Section 3/pizza_order.py
@@ -11,3 +11,10 @@
 # Pepperoni for Medium or Large Pizza: +$3
 
 # Extra cheese for any size pizza: + $1
+
+
+print("Welcome to Python Pizza Deliveries!")
+size = input("What size pizza do you want? S, M, or L ")
+add_pepperoni = input("Do you want pepperoni? Y or N ")
+extra_cheese = input("Do you want extra cheese? Y or N ")
+
