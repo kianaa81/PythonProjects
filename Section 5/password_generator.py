b54e6a18aa305a50880116ee7d196113bcedb316
Kiana <salmasikiana@gmail.com>
2023-06-09 22:50:26 -0600

    Created a for loop for the numbers

--- Section 5/password_generator.py
+++ Section 5/password_generator.py
@@ -21,6 +21,11 @@ for char in range(1, nr_letters + 1):
 for char in range(1, nr_numbers + 1):
     password = random.choice(symbols)
 
+for char in range(1, nr_symbols + 1):
+    password = random.choice(numbers)
+
+print(password)
+
 
 
 #Hard Level - Order of characters randomised:
