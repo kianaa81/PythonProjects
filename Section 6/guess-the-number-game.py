30397087e1b102d321a3ed8b5a7c48ce0eeb6c7a
Kiana <salmasikiana@gmail.com>
2023-07-05 12:46:05 -0600

    Create a global var to deduct the number of turns Function to check user's guess against an actual answer

--- Section 6/guess-the-number-game.py
+++ Section 6/guess-the-number-game.py
@@ -1,1 +1,17 @@
 from random import randint 
+
+
+# Create a global var to deduct the number of turns 
+turns = 0 
+
+# Function to check user's guess against actual answer
+def check_answer(guess, answer, turns):
+    """Checks answer against guess. Returns the number of remaining"""
+    if guess > answer:
+       print("Too high!")
+       return turns - 1 
+    elif guess < answer:
+        print("Too low!")
+        return turns - 1
+    else:
+        print(f"You got it! The answer was {answer}.")
