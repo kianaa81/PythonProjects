106f1f76b43d71bca2b85fe8cef9eba8cd06f7cc
Kiana <salmasikiana@gmail.com>
2023-08-28 20:12:43 -0600

    Put the background image and added title

--- Section 10/main.py
+++ Section 10/main.py
@@ -1,2 +1,19 @@
+from tkinter import *
+
 BACKGROUND_COLOR = "#B1DDC6"
 
+window = Tk()
+window.title("Flashy")
+window.config(padx=50, pady=50, background=BACKGROUND_COLOR)
+
+# Canvas
+canvas = Canvas(width=800, height=526)
+card_front_img = PhotoImage(file="images/card_front.png")
+canvas.create_image(400, 263, image=card_front_img)
+canvas.grid(row=0, column=0)
+canvas.create_text(400, 263, text="word", font=("Ariel", 60, "bold"))
+canvas.config(bg=BACKGROUND_COLOR, highlightthickness=0)
+canvas.create_text(400, 150, text="Title", font=("Ariel", 40, "italic"))
+
+
+window.mainloop()
